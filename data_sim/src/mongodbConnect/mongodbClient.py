c055d49c4d74e40480110333bd3abf29e70b032f
leoutas <hoang.nguyen0@utas.edu.au>
2025-02-21 22:02:33 -0330

    revise connection to mongodb

--- data_sim/src/mongodbConnect/mongodbClient.py
+++ data_sim/src/mongodbConnect/mongodbClient.py
@@ -1,26 +1,26 @@
 import os
 from dotenv import load_dotenv
+import certifi  # to handle SSL certificate verification in order to connect to MongoDB Atlas
 
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 
 # load the specific .env file
 load_dotenv()
 MONGODB_CONNECTION_STRING_LOCAL = os.getenv("MONGODB_CONNECTION_URL_LOCAL")
-MONGODB_CONNECTION_STRING_ONLINE = os.getenv("MONGODB_CONNECTION_URL_ONLINE")
+MONGODB_CONNECTION_STRING_PRODUCTION = os.getenv("MONGODB_CONNECTION_URL_PRODUCTION")
 
+# Create a new client and connect to the server
+client = MongoClient(
+    MONGODB_CONNECTION_STRING_PRODUCTION,
+    server_api=ServerApi("1"),
+    tlsCAFile=certifi.where(),  # to handle SSL certificate verification in order to connect to MongoDB Atlas
+)
 
-# # Create a new client and connect to the server
-# client = MongoClient(MONGODB_CONNECTION_STRING, server_api=ServerApi("1"))
-
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command("ping")
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
-
-
-print(MONGODB_CONNECTION_STRING_LOCAL)
-print(MONGODB_CONNECTION_STRING_ONLINE)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
